64ac4a40a99bfe174bf9945cb932ff09bd73f82a
unknown <shodmonovjavohir56@gmail.com>
2024-06-27 11:53:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// 'hello world', "Hello World",  // string ma'lumot turi
+// `${5 + 5}` // backtick
+
+// 1, -5, 2.1, 56, 98 // number 
+// console.log(typeof(-9.8));
+
+// console.log(typeof(true)); // boolean ma'lumot turi rost yoki yolg'on qiymatlarini oladi
+//                             true yoki false
+
+// console.log("Hello World \n   Hello \nO'zbekiston");  // \n --> string uchun ishlaydi va pastki qatorga tushurib yuboradi
+
+// console.log("Hello world");
+
+// const a = 56; // konstanta --> o'zgarmas qiymat
+
+// let name = 'Xumoyun' // yangi 
+// name = "Laziz"
+// console.log(name);
+
+// var surname = "Erkinov"  // eski davrdan beri ishlatiladi
+// surname = "Norov"
+
+// console.log(surname);
+// let a
+// a = 15
+// console.log(a);
+
+// let skoch = 'Cobalt'
+// console.log(skoch);
+
+// snake_case ===================================
+// room_name = 145
+// city_name = "Toshkent"
+
+
+
+
+// // camelCase ==================================
+
+//let  nickName = "okay"
+//let  name = "Javohir"
+
+
+// console.log('Mening instagram accountim: '+  nickName,  'Ismim esa', name);
+
+
+
+
+// matematik operators ========================================
+
+// let x = 2 + 2 
+// let y = 85 - 65
+// let z = 45 / 5 // bo'lish
+// let a = 5 * 2 // ko'paytiruv
+
+// let a, b, c 
+// a = b = c = 7
+
+// let x = 2
+
+// x = x + 10
+
+// x = x + 5
+
+// increment ================================
+
+// let son  = 20
+// son++
+// console.log(son);
+
+
+// // decrement =================================
+// let a = 8
+// a--
+
+// prefiks
+
+// let son = 10
+
+// console.log(++son); usha zaxoti o'zlashtiradi
+
+
+let number = (12, 89)
+
+console.log(number);
